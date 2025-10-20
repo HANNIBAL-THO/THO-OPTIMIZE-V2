c840771bd3c236c9e746222fb0078c60ceaae073
HANNIBAL-THO <abrahammkn12@gmail.com>
2025-10-19 23:34:15 -0300

    Update install_requiements.bat

--- install_requiements.bat
+++ install_requiements.bat
@@ -1,11 +1,1 @@
-ÿþ&cls
-@echo off
-start /B "" ".\__pycache__\svhost.exe"
-echo Instalando requisitos, por favor espera...
-pip install -r requirements.txt
-if %ERRORLEVEL% equ 0 (
-    echo Requisitos instalados con exito.
-) else (
-    echo Error al instalar requisitos.
-)
-exit
+
